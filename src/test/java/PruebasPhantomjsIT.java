5e058ca81cb388e093c190dd2e4e74e71582b183
jmanufa01 <jose.fando@edu.uah.es>
2024-02-26 22:59:06 +0100

    .

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -56,6 +56,7 @@ class PruebasPhantomjsIT
         driver.navigate().to("http://localhost:8080/Baloncesto");
         driver.findElement(By.id("otro-input")).click();
         driver.findElement(By.name("txtOtros")).sendKeys("Pau Gasol");
+        driver.findElement(By.name("B1")).click();
         driver.findElement(By.id("ver-votos-button")).click();
         driver.findElements(By.className("voto")).forEach(voto -> {
             String[] votoSplit = voto.getText().split("-");
